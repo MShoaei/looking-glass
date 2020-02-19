18ea442f26652fdfa5d8c0eda98bcb49fc5d29d2
Mohammad Shoaei <mohammad.shoaei@outlook.com>
2020-02-20 00:34:32 +0330

    worker API and plugin structure added. JWT token added.

---
+++ worker/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/iris-contrib/middleware/jwt"
+	"github.com/kataras/iris/v12"
+)
+
+var secret []byte
+
+// Runner interface is the interface that plugins should implement
+type Runner interface {
+	Run(params ...string) (stdout io.Writer, stderr io.Writer, err error)
+	Disable()
+	Enable()
+}
+
+func main() {
+	api := iris.Default()
+	j := jwt.New(jwt.Config{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return secret, nil
+		},
+		SigningMethod: jwt.SigningMethodHS512,
+	})
+	api.Post("/login", loginHandler)
+
+	api.Use(j.Serve)
+	api.Get("/home", homeHandler)
+	api.Get("/status", statusHandler)
+
+	// routes to disable or enable a plugin since go plugin package
+	// doesn't support unloading.
+	api.Post("/enable/{plugin:string}", enablePluginHandler)
+	api.Post("/disable/{plugin:string}", disablePluginHandler)
+
+	// /execute accept URL parameters as arguments to commands
+	api.Post("/execute", executeHandler)
+
+	api.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
+}
+
+func loginHandler(context iris.Context) {
+	token := jwt.NewTokenWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+		"iat": time.Now().Unix(),
+	})
+	signedToken, err := token.SignedString(secret)
+	if err != nil {
+		context.StatusCode(iris.StatusInternalServerError)
+		context.JSON(iris.Map{
+			"summary": "there was a problem generating token",
+			"error":   err,
+		})
+	}
+	context.StatusCode(iris.StatusOK)
+	context.JSON(iris.Map{
+		"token": fmt.Sprintf("Bearer %s", signedToken),
+	})
+}
+
+func homeHandler(context iris.Context) {
+	context.JSON(iris.Map{
+		"message": "success",
+	})
+}
+
+func statusHandler(context iris.Context) {
+
+}
+
+func enablePluginHandler(context iris.Context) {
+
+}
+
+func disablePluginHandler(context iris.Context) {
+
+}
+
+func executeHandler(context iris.Context) {
+
+}
+
+func init() {
+	secret = []byte(os.Getenv("SECRET"))
+	if len(secret) == 0 {
+		secret = []byte(`YX<_RDS'K%"qOWDy*z*|rKDn&0|k<8`)
+	}
+
+}
