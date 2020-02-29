873db76edcb7e6e0dc99e1ae14a25f951b6801c6
Mohammad Shoaei <mohammad.shoaei@outlook.com>
2020-03-01 02:23:58 +0330

    pretty print

--- database/db.sql
+++ database/db.sql
@@ -1,7 +1,10 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY UNIQUE NOT NULL,
-    username varchar(50) UNIQUE NOT NULL,
-    password varchar(97) NOT NULL
+CREATE TABLE users
+(
+    id       SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    username varchar(50) UNIQUE        NOT NULL,
+    password varchar(97)               NOT NULL
 );
 
-INSERT INTO users(username, password) VALUES ('admin', '$argon2id$v=19$m=65536,t=3,p=2$UDQbl6KSK7h0nzhb/eAAtw$e3ZT9jPEprmoP1A40wD04a5s0J5/2otW1gOdny5M9/E'); -- admin, testadminpassword
+INSERT INTO users(username, password)
+VALUES ('admin',
+        '$argon2id$v=19$m=65536,t=3,p=2$UDQbl6KSK7h0nzhb/eAAtw$e3ZT9jPEprmoP1A40wD04a5s0J5/2otW1gOdny5M9/E'); -- admin, testadminpassword
